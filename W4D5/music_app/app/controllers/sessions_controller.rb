81fa5c86949bd682a05ed217bcbd054cc2cccb64
Samuel Chia <cheeeya@berkeley.edu>
2018-01-05 13:54:58 -0800

    phase 0

---
+++ W4D5/music_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if user
+      user.reset_session_token!
+      login!(user)
+      redirect_to user_url(user)
+    else
+      render plain: "Invalid Username or Password"
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to users_url
+  end
+end
