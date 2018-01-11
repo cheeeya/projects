60b66854d0bf9b0b8e291de7d0dd5407d9a51cbb
Samuel Chia <cheeeya@berkeley.edu>
2018-01-10 17:25:02 -0800

    finish phase 1

---
+++ W5D3/skeleton/bin/p01_basic_server.rb
@@ -0,0 +1,14 @@
+require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-type'] = 'text/html'
+  res.write(env["REQUEST_PATH"])
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
